7f6494505d47cc8077c1bb043a39de204949d26e
Yumi Izumi <mail@yumi.to>
2025-02-17 08:51:51 +0900

    Update homepage URL in xcode-discord-rpc formula

--- Formula/xcode-discord-rpc.rb
+++ Formula/xcode-discord-rpc.rb
@@ -1,6 +1,6 @@
 class XcodeDiscordRpc < Formula
   desc "A simple Discord Rich Presence client for Xcode"
-  homepage "https://github.com/izyumidev/xcode-discord-rpc"
+  homepage "https://github.com/izyuumi/xcode-discord-rpc"
   url "https://github.com/izyuumi/xcode-discord-rpc/releases/download/v0.3.3/xcode-discord-rpc.tar.gz"
   sha256 "59848d2b99020d457d86d4e53242dc33cace2447c8ee49faf645c08a545a2e75"
   version "0.3.3"
